ff3d53132f15b67d171dd3ae4b6b509786c5e2c3
nakahiro642 <ktc25a32j0005@edu.kyoto-tech.ac.jp>
2025-11-29 11:36:48 +0900

    コンポーネントのスタイルを更新し、サムネイル画像の表示を追加。メンバーリストとニュースリストの取得機能を実装し、定数を新規作成。ページコンポーネントを修正してデータを動的に取得。

--- app/_components/NewsList/index.tsx
+++ app/_components/NewsList/index.tsx
@@ -19,18 +19,29 @@ export default function NewsList({ news }: Props) {
             {news.map((article) => (
                 <li key={article.id} className={styles.List}>
                     <Link href={`/news/${article.id}`} className={styles.link}>
-                        <Image 
-                          className={styles.image}
-                          src="/no-image.png"
-                          alt="NO Image"
-                          width={1200}
-                          height={630}
-                        />
+
+                    { article.thumbnail ? (
+                        <Image
+                            src={article.thumbnail.url}
+                            alt=""
+                            className={styles.thumbnail}
+                            width={article.thumbnail.width}
+                            height={article.thumbnail.height}
+                            />
+                    ) : (
+                        <Image
+                            className={styles.image}
+                            src="/no-image.png"
+                            alt="No Image"
+                            width={1200}
+                            height={630}
+                        /> 
+                    )}
                         <dl className={styles.content}>
                             <dt className={styles.title}>{article.title}</dt>
                             <dd className={styles.meta}>
                                 <Category category={article.category} />
-                                <Date date={article.publishedAt} />
+                                {article.publishedAt && <Date date={article.publishedAt} />}
                             </dd>
                         </dl>
                     </Link>
