19c3d5d7af69d580f56bf2d5b3b3b0d2f99f83fb
nakahiro642 <ktc25a32j0005@edu.kyoto-tech.ac.jp>
2025-12-13 12:30:29 +0900

    お問い合わせフォームを新規作成し、バリデーション機能を追加

---
+++ app/_components/ContactForm/index.tsx
@@ -0,0 +1,33 @@
+import styles from "./index.module.css";
+
+export default function ContactForm() {
+    return (
+        <form className={styles.form}>
+            <div className={styles.horizontal}>
+                <div className={styles.item}>
+                    <label className={styles.label} htmlFor="lastname">姓</label>
+                    <input className={styles.textfield} type="text" id="lastname" name="lastname" />
+                </div>
+                <div className={styles.item}>
+                    <label className={styles.label} htmlFor="firstname">名</label>
+                    <input className={styles.textfield} type="text" id="firstname" name="firstname" />
+                </div>
+            </div>
+            <div className={styles.item}>
+                <label className={styles.label} htmlFor="company">会社名</label>
+                <input className={styles.textfield} type="text" id="company" name="company" />
+            </div>
+            <div className={styles.item}>
+                <label className={styles.label} htmlFor="email">メールアドレス</label>
+                <input className={styles.textfield} type="text" id="email" name="email" />
+            </div>
+            <div className={styles.item}>
+                <label className={styles.label} htmlFor="messsage">メッセージ</label>
+                <textarea className={styles.textfield} id="message" name="message"></textarea>
+            </div>
+            <div className={styles.actions}>
+                <input type="submit" value="送信する" className={styles.button} />
+            </div>
+        </form>
+    );
+}
