88c995e4d16eac7d135f0a7520d0badd02c74147
nakahiro642 <ktc25a32j0005@edu.kyoto-tech.ac.jp>
2025-12-13 12:39:21 +0900

    お問い合わせフォームにバリデーション機能を追加し、エラーメッセージを表示するように修正

--- app/_components/ContactForm/index.tsx
+++ app/_components/ContactForm/index.tsx
@@ -1,8 +1,27 @@
+"use client";
+
+import { createContactData } from "@/app/_actions/contact";
+import { useFormState } from "react-dom";
 import styles from "./index.module.css";
 
+const initialState = {
+    status: "",
+    message: "",
+};
+
 export default function ContactForm() {
+    const [state, formAction] = useFormState(createContactData, initialState);
+    console.log(state);
+    if (state.status === "success") {
+        return (
+            <p className={styles.success}>
+                お問い合わせいただき、ありがとうございます。<br />
+                お返事まで今しばらくお待ちください。
+            </p>
+        );
+    }
     return (
-        <form className={styles.form}>
+        <form className={styles.form} action={formAction}>
             <div className={styles.horizontal}>
                 <div className={styles.item}>
                     <label className={styles.label} htmlFor="lastname">姓</label>
@@ -26,8 +45,13 @@ export default function ContactForm() {
                 <textarea className={styles.textfield} id="message" name="message"></textarea>
             </div>
             <div className={styles.actions}>
+                {state.status === "error" && (
+                    <p className={styles.error}>{state.message}</p>
+                )}
                 <input type="submit" value="送信する" className={styles.button} />
             </div>
         </form>
     );
 }
+
+//251から
